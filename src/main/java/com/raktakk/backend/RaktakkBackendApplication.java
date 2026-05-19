396d8570c39940640bc6236049852d45feb290e1
BMD0910 <bmd09102000@gmail.com>
2026-05-19 19:11:14 +0000

    config variable 4

--- src/main/java/com/raktakk/backend/RaktakkBackendApplication.java
+++ src/main/java/com/raktakk/backend/RaktakkBackendApplication.java
@@ -1,6 +1,5 @@
 package com.raktakk.backend;
 
-import com.raktakk.backend.config.RailwayDatasourceBootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RaktakkBackendApplication {
 
     public static void main(String[] args) {
-        RailwayDatasourceBootstrap.apply();
         SpringApplication.run(RaktakkBackendApplication.class, args);
     }
 }
